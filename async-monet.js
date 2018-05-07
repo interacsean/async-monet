d55a1e93a14b7a55d2a944fbb5aa6817b8dbe73d
Sean Hasselback <sean.hasselback@stationfive.com>
2018-05-07 11:51:46 +1000

    WIP on es6 and flow compatiblility

--- async-monet.js
+++ async-monet.js
@@ -1,48 +1,80 @@
-const monet = require('monet');
+// @flow
 
-// Create leftBind / leftFlatMap
-monet.Either.fn.leftBind = function(fn) {
-  return this.isLeft() ? fn(this.value) : this;
-}
-monet.Either.fn.leftFlatMap = monet.Either.fn.leftBind;
+/* eslint no-proto: 0 */
+/* eslint no-extend-native: 0 */
 
-// map which awaits thenable value
-monet.Either.fn.awaitMap = function (fn) {
-  if (this.isRightValue) {
-    const result = fn(this.value);
-    if (typeof result.then === 'function') {
-      return new Promise((res, rej) => {
-        result
-          .then(resVal => res(monet.Either.Right(resVal)))
-          .catch(rejVal => rej(monet.Either.Right(rejVal)))
-      })
-    }
-    return monet.Either.Right(result);
-  }
-  return this;
-}
+import monet from 'monet';
 
-const mFuncs = [
-  'map',
-  'leftBind',
-  'leftFlatMap',
-  'awaitMap',
-  'flatMap',
-  'bind',
-  'toEither',
-  'toMaybe',
-  'cata',
-  'bimap',
-  'left',
-  'right',
-  'isLeft',
-  'isRight',
-];
-
-mFuncs.forEach(mFunc => {
-  Promise.prototype[mFunc] = function(f) {
-    return this.then((resVal) => resVal.__proto__[mFunc].call(resVal, f));  
+export const extendMonads: Function = () => {
+  monet.Either.fn.leftBind = function leftBind(fn: Function): mixed {
+    return this.isLeft() ? fn(this.value) : this;
   };
-})
+  monet.Either.fn.leftFlatMap = monet.Either.fn.leftBind;
 
-module.exports = monet;
+  monet.Either.fn.awaitMap = function awaitMap(fn: Function): mixed {
+    return this.flatMap((val: mixed): mixed => {
+      const result: mixed = fn(this.value || this.val);
+      if (typeof result.then === 'function') {
+        return new Promise((res: Function, rej: Function) => {
+          result
+            .then((resVal: mixed) => {
+              res(this.of(resVal));
+            })
+            .catch((rejVal: mixed) => {
+              rej(this.of(rejVal));
+            });
+        });
+      }
+      return this.of(result);
+    });
+  };
+  monet.Maybe.fn.awaitMap = monet.Either.fn.awaitMap;
+
+  monet.Either.fn.tap = function tap(fn: Function): mixed {
+    return this.map((original: mixed): mixed => {
+      fn(original);
+      return original;
+    });
+  };
+  monet.Maybe.fn.tap = monet.Either.fn.tap;
+};
+
+export const extendPromises: Function = () => {
+  const mFuncs: Array<string> = [
+    'map',
+    'flatMap',
+    'leftMap',
+    'bind',
+    'toEither',
+    'toMaybe',
+    'cata',
+    'bimap',
+    'left',
+    'right',
+    'isLeft',
+    'isRight',
+    'leftBind',
+    'leftFlatMap',
+    'awaitMap',
+    'tap',
+  ];
+
+  mFuncs.forEach((mFunc: string) => {
+    Promise.prototype[mFunc] = function mapPromiseMonadFn(...args: Array<mixed>): Promise<mixed> {
+      return this.then((resVal: Object): mixed => {
+        if (!resVal.__proto__[mFunc]) {
+          throw new Error(`function ${mFunc} not defined in asyncMonet.`);
+        }
+        return resVal.__proto__[mFunc].apply(resVal, args);
+      });
+    };
+  });
+};
+
+if (!monet.Either.fn.leftBind) {
+  extendMonads();
+}
+
+if (!Promise.prototype.map) {
+  extendPromises();
+}
--- spec/async-monet.spec.js
+++ spec/async-monet.spec.js
@@ -1,112 +1,191 @@
+require('../async-monet');
 const {
   Either,
   Maybe,
-} = require('../async-monet');
+} = require('monet');
 
 const delay = (val, shouldRes = true) => new Promise((res, rej) =>
   setTimeout(() => shouldRes ? res(val) : rej(val), 500));
 
-describe('Standard Either', () => {
-  it('Can still map synchronously (as per original monet)', async (done) => {
-    const either = Either.Right('value');
-    
-    const result = either
-    .map((val) => `${val} mapped`)
-    .map((val) => `${val} twice`);
-  
-    expect(result.isRight()).toBe(true);
-    expect(result.right()).toEqual('value mapped twice');
-    
-    done();
+describe('Async Monads', () => {
+  describe('Standard Either', () => {
+    it('Can still map synchronously (as per original monet)', async (done) => {
+      const either = Either.Right('value');
+
+      const result = either
+        .map(val => `${val} mapped`)
+        .map(val => `${val} twice`);
+
+      expect(result.isRight()).toBe(true);
+      expect(result.right()).toEqual('value mapped twice');
+
+      done();
+    });
+
+    it('Can still flatMap syncronously', () => {
+      const either = Either.Right('value');
+
+      const result = either
+        .flatMap(val => Either.Right(`${val} flatMapped`))
+        .flatMap(val => `${val} twice`);
+
+      expect(result).toEqual('value flatMapped twice');
+    });
   });
 
-  it('Can still flatMap syncronously', () => {
-    const either = Either.Right('value');
+  describe('Async Either', () => {
+    it('Can map asynchronously', async (done) => {
+      const either = Either.Right('value');
 
-    const result = either
-      .flatMap(val => Either.Right(`${val} flatMapped`))
-      .flatMap(val => `${val} twice`);
+      const result = await either
+        .awaitMap(val => delay(`${val} mapped`))
+        .awaitMap(val => delay(`${val} twice`));
 
-    expect(result).toEqual('value flatMapped twice');
-  });
-});
+      expect(result.isRight()).toBe(true);
+      expect(result.right()).toEqual('value mapped twice');
 
-describe('Async Either', () => {
-  it('Can map asynchronously', async (done) => {
-    const either = Either.Right('value');
+      done();
+    });
 
-    const result = await either
-      .awaitMap((val) => delay(`${val} mapped`))
-      .awaitMap((val) => delay(`${val} twice`));
+    it('Map asynchronously is bypassed on left', async (done) => {
+      const either = Either.Left('value');
 
-    expect(result.isRight()).toBe(true);
-    expect(result.right()).toEqual('value mapped twice');
+      const result = await either
+        .awaitMap(val => delay(`${val} mapped`))
+        .awaitMap(val => delay(`${val} twice`));
 
-    done();
-  });
+      expect(result.isLeft()).toBe(true);
+      expect(result.left()).toEqual('value');
 
-  it('Map asynchronously is bypassed on left', async (done) => {
-    const either = Either.Left('value');
+      done();
+    });
 
-    const result = await either
-      .awaitMap((val) => delay(`${val} mapped`))
-      .awaitMap((val) => delay(`${val} twice`));
+    it('Can flatMap asynchronously', async (done) => {
+      const either = Either.Right('value');
 
-    expect(result.isLeft()).toBe(true);
-    expect(result.left()).toEqual('value');
+      const result = await either
+        .flatMap(val => delay(Either.Right(`${val} flatMapped`)))
+        .flatMap(val => `${val} twice`);
 
-    done();
-  });
+      expect(result).toEqual('value flatMapped twice');
 
-  it('Can flatMap asynchronously', async (done) => {
-    const either = Either.Right('value');
+      done();
+    });
 
-    const result = await either
-      .flatMap((val) => delay(Either.Right(`${val} flatMapped`)))
-      .flatMap(val => `${val} twice`);
+    it('flatMap bypasses left', async (done) => {
+      const either = Either.Right('value');
 
-    expect(result).toEqual('value flatMapped twice');
+      const result = await either
+        .flatMap(val => delay(Either.Left(`${val} flatMapped once`)))
+        .flatMap(val => `${val} not twice`);
 
-    done();
-  });
+      expect(result.isLeft()).toBe(true);
+      expect(result.left()).toEqual('value flatMapped once');
 
-  it('flatMap bypasses left', async (done) => {
-    const either = Either.Right('value');
-
-    const result = await either
-      .flatMap((val) => delay(Either.Left(`${val} flatMapped once`)))
-      .flatMap(val => `${val} not twice`);
-
-    expect(result.isLeft()).toBe(true);
-    expect(result.left()).toEqual('value flatMapped once');
-
-    done();
-  });
-});
-
-describe('Left bind', () => {
-  it('binds on lefts', async (done) => {
-    const either = Either.Left('value');
-
-    const result = await either
-      .leftBind((val) => delay(Either.Left(`${val} leftBound`)))
-      .leftFlatMap(val => `${val} twice`);
-
-    expect(result).toEqual('value leftBound twice');
-
-    done();
+      done();
+    });
   });
 
-  it('does not bind on rights', async (done) => {
-    const either = Either.Left('value');
+  describe('Either left bind', () => {
+    it('binds on lefts', async (done) => {
+      const either = Either.Left('value');
 
-    const result = await either
-      .leftBind((val) => delay(Either.Right(`${val} leftBound once`)))
-      .leftFlatMap(val => `${val} not twice`);
+      const result = await either
+        .leftBind(val => delay(Either.Left(`${val} leftBound`)))
+        .leftFlatMap(val => `${val} twice`);
 
-    expect(result.isRight()).toBe(true);
-    expect(result.right()).toEqual('value leftBound once');
+      expect(result).toEqual('value leftBound twice');
 
-    done();
+      done();
+    });
+
+    it('does not bind on rights', async (done) => {
+      const either = Either.Left('value');
+
+      const result = await either
+        .leftBind(val => delay(Either.Right(`${val} leftBound once`)))
+        .leftFlatMap(val => `${val} not twice`);
+
+      expect(result.isRight()).toBe(true);
+      expect(result.right()).toEqual('value leftBound once');
+
+      done();
+    });
+  });
+
+  describe('Standard Maybe', () => {
+    it('Can still map synchronously (as per original monet)', async (done) => {
+      const maybe = Maybe.Some('value');
+
+      const result = maybe
+        .map(val => `${val} mapped`)
+        .map(val => `${val} twice`);
+
+      expect(result.isSome()).toBe(true);
+      expect(result.some()).toEqual('value mapped twice');
+
+      done();
+    });
+
+    it('Can still flatMap syncronously', () => {
+      const maybe = Maybe.Some('value');
+
+      const result = maybe
+        .flatMap(val => Maybe.Some(`${val} flatMapped`))
+        .flatMap(val => `${val} twice`);
+
+      expect(result).toEqual('value flatMapped twice');
+    });
+  });
+
+  describe('Async Maybe', () => {
+    it('Can map asynchronously', async (done) => {
+      const maybe = Maybe.Some('value');
+
+      const result = await maybe
+        .awaitMap(val => delay(`${val} mapped`))
+        .awaitMap(val => delay(`${val} twice`));
+
+      expect(result.isSome()).toBe(true);
+      expect(result.some()).toEqual('value mapped twice');
+
+      done();
+    });
+
+    it('Map asynchronously is bypassed on none', async (done) => {
+      const maybe = Maybe.None('value');
+
+      const result = await maybe
+        .awaitMap(val => delay(`${val} mapped`))
+        .awaitMap(val => delay(`${val} twice`));
+
+      expect(result.isNone()).toBe(true);
+
+      done();
+    });
+
+    it('Can flatMap asynchronously', async (done) => {
+      const maybe = Maybe.Some('value');
+
+      const result = await maybe
+        .flatMap(val => delay(Maybe.Some(`${val} flatMapped`)))
+        .flatMap(val => `${val} twice`);
+
+      expect(result).toEqual('value flatMapped twice');
+
+      done();
+    });
+
+    it('flatMap bypasses none', async (done) => {
+      const maybe = Maybe.Some('value');
+
+      const result = await maybe
+        .flatMap(val => delay(Maybe.None(`${val} flatMapped once`)))
+        .flatMap(val => `${val} not twice`);
+
+      expect(result.isNone()).toBe(true);
+
+      done();
+    });
   });
 });
